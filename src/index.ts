4ccb3442fbe9a93f5ac7082131ff7bab9696fac6
Samuel Ybarra <sybarra2076@gmail.com>
2026-02-04 15:19:28 -0600

    Adding readiness handler in api folder

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.get("/healthz", handlerReadiness);
