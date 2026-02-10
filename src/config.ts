bd902f281c12031c79ade40d99a4d7e19881cf69
Samuel Ybarra <sybarra2076@gmail.com>
2026-02-10 12:44:42 -0600

    Adding drizzle

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
+process.loadEnvFile();
+
 type APIConfig = {
     fileServerHits: number,
+    dbURL: string,
 };
 
 export const config: APIConfig = {
     fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL"),
+}
+
+function envOrThrow(key: string){
+    if(!process.env[key]){
+        throw new Error("DB_URL not set");
+    }
+    return process.env[key];
 }
